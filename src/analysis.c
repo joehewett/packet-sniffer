b4c621f97278042b14847e14a561c34fd6cc1592
Joe Hewett <joehewett1@gmail.com>
2020-11-09 12:24:42 +0000

    Initial skeleton

---
+++ src/analysis.c
@@ -0,0 +1,12 @@
+#include "analysis.h"
+
+#include <pcap.h>
+#include <netinet/if_ether.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+
+void analyse(struct pcap_pkthdr *header,
+             const unsigned char *packet,
+             int verbose) {
+  // TODO your part 2 code here
+}
