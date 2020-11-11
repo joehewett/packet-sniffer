518aaedbc29acbd53107940835531ebeed21500b
Joe Hewett <joehewett1@gmail.com>
2020-11-11 14:18:25 +0000

    Testing

--- src/analysis.c
+++ src/analysis.c
@@ -9,7 +9,7 @@
 void analyse(struct pcap_pkthdr *header, const unsigned char *packet, int verbose) {
   // TODO your part 2 code here
   
-  struct ether_header * eth_header = (struct ether_header *) header;
+  struct ether_header * eth_header = (struct ether_header *) packet;
   unsigned short ethernet_type = ntohs(eth_header->ether_type);
   unsigned short ethernet_desthost = ntohs(eth_header->ether_dhost);
   unsigned short ethernet_sourcehost = ntohs(eth_header->ether_shost);
