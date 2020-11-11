0fea929712da487433f145c9f96c1a55829d6f81
Joe Hewett <joehewett1@gmail.com>
2020-11-11 12:08:08 +0000

    Attempt to print syn bit for TCP headers

--- src/analysis.c
+++ src/analysis.c
@@ -4,9 +4,16 @@
 #include <netinet/if_ether.h>
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
+#include <netinet/in.h>
 
-void analyse(struct pcap_pkthdr *header,
-             const unsigned char *packet,
-             int verbose) {
+void analyse(struct pcap_pkthdr *header, const unsigned char *packet, int verbose) {
   // TODO your part 2 code here
+  
+  struct ether_header * eth_header = (struct ether_header *) header;
+  unsigned short ethernet_type = ntohs(eth_header->ether_type);
+  printf("\nType: %hu\n", ethernet_type);
+
+  struct tdphdr * tcp_header = (struct tcphdr *) header; 
+  printf("SYN FLAG is %u\n", (unsigned int)TH_SYN);
+
 }
